199abc2ea33336f586bf2a7ff358d662b1e70ddd
Someone52r <urcenkovandrej@gmail.com>
2025-10-15 17:47:30 +0300

    modified, give output res sa summ of all calculated U

--- lab1.c
+++ lab1.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <math.h>
-float U;
-int main() {
+int main(float U, float res) {
+    res = 0;
     for (float x=1; x<=2; x=x+0.3) {
         for (float y=2; y<=2.5; y=y+0.5) {
             if (x/(y*y)<1) {
                 U=fmax(cos(pow(x,3)-sqrt(y)),cbrt(x*pow(y,2)));
                 printf ("%f\n", U);
+                res=res+U;
             } else {
                 U=log(pow(y,2)-x);
                 printf ("%f\n", U);
+                res=res+U;
             }
         }
     }
+    printf ("%f\n", res);
     return 0;
 }
